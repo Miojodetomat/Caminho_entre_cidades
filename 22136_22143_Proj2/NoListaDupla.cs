f8b107dc65f1078b8cc92d706200d2879891d020
HUGO GOMES SOARES <u22136@academico.cotuca.unicamp.br>
2023-05-23 10:18:37 -0300

    oi

---
+++ 22136_22143_Proj2/NoListaDupla.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class NoListaDupla<Dado> where Dado : IComparable<Dado>, IRegistro
+{
+    NoListaDupla<Dado> anterior;
+    Dado info;
+    NoListaDupla<Dado> proximo;
+
+    public NoListaDupla(Dado info)
+    {
+        this.info = info;
+        anterior = proximo = null;
+    }
+
+    public Dado Info
+    {
+        get => info;
+        set => info = value;
+    }
+    public NoListaDupla<Dado> Proximo { get => proximo; set => proximo = value; }
+    public NoListaDupla<Dado> Anterior { get => anterior; set => anterior = value; }
+}
+
